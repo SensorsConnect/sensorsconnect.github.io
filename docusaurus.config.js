21909085d3163b089590463c605e9855771a4020
elewah <abdelrahman.elewah@gmail.com>
2024-07-13 20:56:10 -0400

    adding under construction sign in the docs page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'SensorsConnect',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Under Construction',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
